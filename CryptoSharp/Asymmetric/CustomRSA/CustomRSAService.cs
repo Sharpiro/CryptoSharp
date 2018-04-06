b80736007d83e8074b07d0fb0eb7df79b6daaac1
sharpiro <dsharpbb09@gmail.com>
2018-04-06 04:53:46 -0400

    adding simple RSA/AES

---
+++ CryptoSharp/Asymmetric/CustomRSA/CustomRSAService.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace CryptoSharp.Asymmetric.CustomRSA
+{
+    public class CustomRSAService
+    {
+        public int KeySizeBits { get; private set; }
+        public BigInteger EStart { get; set; } = 3;
+        public BigInteger E { get; private set; }
+        public BigInteger P { get; private set; }
+        public BigInteger Q { get; private set; }
+        public BigInteger N { get; private set; }
+        public BigInteger M { get; private set; }
+        public BigInteger D { get; private set; }
+
+        private CustomRSAService() { }
+
+        public static CustomRSAService Create(int keySizeBits)
+        {
+            var rsaService = new CustomRSAService();
+            rsaService.KeySizeBits = keySizeBits;
+            rsaService.Initialize();
+            return rsaService;
+        }
+
+        public static CustomRSAService Create(BigInteger e, BigInteger n, BigInteger d = default(BigInteger))
+        {
+            var rsaService = new CustomRSAService
+            {
+                E = e,
+                N = n,
+                D = d
+            };
+            return rsaService;
+        }
+
+        private static BigInteger GetRandomPrime(int size, BigInteger e)
+        {
+            var number = new BigInteger();
+            var randomizer = new RNGCryptoServiceProvider();
+            var buffer = new byte[size + 1];
+            while (!number.IsProbablePrime(100))
+            {
+                randomizer.GetBytes(buffer);
+                buffer[0] = 1;
+                buffer[size] = 0;
+                number = new BigInteger(buffer);
+            }
+            return number;
+        }
+
+        private void Initialize()
+        {
+            if (P == 0)
+            {
+                P = GetRandomPrime(KeySizeBits, EStart);
+            }
+            if (Q == 0)
+            {
+                Q = GetRandomPrime(KeySizeBits, EStart);
+            }
+            N = P * Q;
+            E = EStart;
+            M = (P - 1) * (Q - 1);
+            while (GCD(E, M) > 1)
+            {
+                E += 2;
+            }
+            D = ModInverse(E, M);
+        }
+
+        public byte[] Encrypt(byte[] plainBytes)
+        {
+            var plainInteger = new BigInteger(plainBytes);
+            var encrypted = BigInteger.ModPow(plainInteger, E, N);
+            var cryptoBytes = encrypted.ToByteArray();
+            return cryptoBytes;
+        }
+
+        public byte[] Decrypt(byte[] cryptoBytes)
+        {
+            if (D <= 0) throw new Exception("No private key provided, D must be greater than 0");
+
+            var cryptoInteger = new BigInteger(cryptoBytes);
+            var plain = BigInteger.ModPow(cryptoInteger, D, N);
+            var plainBytes = plain.ToByteArray();
+            return plainBytes;
+        }
+
+        private static BigInteger GCD(BigInteger a, BigInteger b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            return a == 0 ? b : a;
+        }
+
+        private static BigInteger ModInverse(BigInteger a, BigInteger n)
+        {
+            BigInteger t = new BigInteger(0), newt = new BigInteger(1);
+            BigInteger r = n, newr = a;
+            while (newr != 0)
+            {
+                var quotient = r / newr;
+                var tempT = t;
+                t = newt;
+                newt = tempT - quotient * newt;
+                var tempR = r;
+                r = newr;
+                newr = tempR - quotient * newr;
+            }
+            if (r > 1)
+            {
+                return -1;
+            }
+            if (t < 0)
+            {
+                t = t + n;
+            }
+            return t;
+        }
+
+        private static BigInteger GetRandomNumberLessThan(BigInteger x, int maxBytes, Random randomizer)
+        {
+            var number = x;
+            var buffer = new byte[maxBytes + 1];
+            while (number >= x || number == 0)
+            {
+                randomizer.NextBytes(buffer);
+                buffer[maxBytes] = 0;
+                number = new BigInteger(buffer);
+            }
+            return number;
+        }
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,56 @@
+﻿using CryptoSharp.Asymmetric.CustomRSA;
+using CryptoSharp.Symmetric;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using static System.Console;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var testBytes = new byte[] { 1, 2, 3 };
+            //(var key, var iv) = SimpleAes.CreateKey();
+            //var plainBytes = key.Concat(iv).ToArray();
+            //var plainHex = string.Join(string.Empty, plainBytes.Select(b => b.ToString("x2")));
+            var plainHex = "fb86b909a356da4b639ad0a767d0595d3119440a1eb9e2c504ee102c57af497f5326bf6a7d08bdbb17019d6407c77be9";
+            var plainBytes = GetBytesFromHex(plainHex);
+            //Debug.Assert(plainBytes.Length == 32 + 16);
+            //var cryptoBytes = SimpleAes.Encrypt(testBytes, key, iv);
+            //var decryptedBytes = SimpleAes.Decrypt(cryptoBytes, key, iv);
+            //Debug.Assert(testBytes.SequenceEqual(decryptedBytes));
+
+            var e = new BigInteger(7);
+            var n = BigInteger.Parse("522806553487135022947979710062295163573133494999739899086216549037195002661539576520853599318297663743426372773771723005330074567544450324957918152900097");
+            var d = BigInteger.Parse("373433252490810730677128364330210831123666782142671356490154677883710716186756589892531398008495512038063359957925071200972864532192059518154993437666743");
+            var rsaService = CustomRSAService.Create(e, n, d);
+            //var rsaService = CustomRSAService.Create(32);
+            //var plainBytes = GetBytesFromHex("dd3e0fc7784d45f99e789bd94058fc3add3e0fc7784d45f99e789bd94058fc3a");
+            var cryptoBytes = rsaService.Encrypt(plainBytes);
+            //var cryptoBase64 = Convert.ToBase64String(cryptoBytes);
+            var cryptoBase64 = "sthvYiX3MDIjr9SEXPOn1IHmpay7QtYsLEjWwQrYUGNzUj6LRTrkoQQ1Ct/B587nVsNP7O/lWKzeWoCn4mPL+A==";
+            //var decryptedBytes = rsaService.Decrypt(cryptoBytes);
+            //var plainText = string.Join(string.Empty, decryptedBytes.Select(b => b.ToString("x2")).ToArray());
+            //WriteLine(plainText);
+            //Debug.Assert(plainBytes.SequenceEqual(decryptedBytes));
+
+            WriteLine("done");
+            ReadKey();
+        }
+
+        public static byte[] GetBytesFromHex(string hexString)
+        {
+            var bytes = new byte[hexString.Length / 2];
+            for (int i = 0, j = 0; i <= hexString.Length; i++)
+            {
+                if (i == 0 || i % 2 != 0) continue;
+                var stringByte = $"{hexString[i - 2]}{hexString[i - 1]}";
+                bytes[j++] = Convert.ToByte(stringByte, 16);
+            }
+            return bytes;
+        }
+    }
+}
